6cf44af4a5251ddef5846aa04ba9a772e6055b6e
weirdlo <25072633+weirdlo@users.noreply.github.com>
2025-03-20 15:29:44 -0400

    Added method to read files.

--- FileHandler/src/fileHandler/FileStorage.java
+++ FileHandler/src/fileHandler/FileStorage.java
@@ -1,6 +1,7 @@
 package fileHandler;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
@@ -8,9 +9,6 @@ import java.util.Scanner;
 public class FileStorage {
 	public void createFile(String name) {
 		try {
-			/**
-			 * Need to implement a method that will check to see what kind of file we are creating (.txt, .pdf, .word, etc.).
-			 */
 			String filename = name+".txt";
 			File file = new File(filename);
 			
@@ -34,9 +32,9 @@ public class FileStorage {
 			
 			//To make the filename checks case sensitive.
 			String canonicalPath = file.getCanonicalPath();
-			String editFile =  canonicalPath.substring(canonicalPath.lastIndexOf(File.separator) + 1);
+			String checkName =  canonicalPath.substring(canonicalPath.lastIndexOf(File.separator) + 1);
 			
-			if(file.exists() && editFile.equals(file.getName())) {
+			if(file.exists() && checkName.equals(file.getName())) {
 				String text;
 				FileWriter scribe = new FileWriter(file);
 				
@@ -57,4 +55,29 @@ public class FileStorage {
 			e.printStackTrace();
 		}//end of catch
 	}//end of editFile
+	
+	public void readFile(String name){
+		try {
+			String filename = name;
+			File file = new File(filename);
+			
+			String readPath = file.getCanonicalPath();
+			String checkFile =  readPath.substring(readPath.lastIndexOf(File.separator) + 1);
+			
+			Scanner reader = new Scanner(file);
+			
+			if(file.exists() && checkFile.equals(file.getName())) {
+				while(reader.hasNextLine()){
+					String lines = reader.nextLine();
+					System.out.println(lines);
+				}
+			}
+			else {
+				System.out.println("The file you've entered does not exist.");
+			}
+		}
+		catch(IOException e) {
+			System.out.println("An error occured while trying to read the file.");
+		}
+	}//end of readFile
 }//end of class
